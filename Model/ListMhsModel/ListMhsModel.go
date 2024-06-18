843a191cd4ba8e19c55d630db6f366df7e0577da
raffiMRG <mutekinoraffi@gmail.com>
2024-06-18 22:48:17 +0700

    add new API for get and post

--- Model/ListMhsModel/ListMhsModel.go
+++ Model/ListMhsModel/ListMhsModel.go
@@ -1,17 +1,14 @@
 package ListMhsModel
 
 type ListMhs struct {
-	// KTPID      string `json:"ktpId" form:"ktpId"`
-	// NamaTamu   string `json:"namaTamu" form:"namaTamu"`
-	// Tujuan     string `json:"tujuan" form:"tujuan"`
-	// Keterangan string `json:"keterangan" form:"keterangan"`
-	ID         int    `json:"id" form:"id"`
-	UUID_CARD  string `json:"uuid_card" form:"uuid_card"`
-	Nama_mhs   string `json:"nama_mhs" form:"nama_mhs"`
-	CREATED_AT string `json:"created_at" form:"created_at"`
+	ID        int    `json:"id" form:"id"`
+	UUID_CARD string `json:"uuid_card" form:"uuid_card"`
+	NIM       string `json:"nim" form:"nim"`
+	Nama_mhs  string `json:"nama_mhs" form:"nama_mhs"`
+	CREATE_AT string `json:"create_at" form:"create_at"`
 }
 
 // TableName memberikan nama tabel yang eksplisit
 func (ListMhs) TableName() string {
-	return "tb_card"
+	return "tb_akun"
 }
